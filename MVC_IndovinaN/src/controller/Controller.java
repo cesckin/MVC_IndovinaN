a822d1791a118cd94a9e6368a07c38d1cd5eb3fe
Francesco <Francesco@DESKTOP-BRI3K8A>
2021-09-25 10:33:34 +0200

    Controlla testo

--- MVC_IndovinaN/src/controller/Controller.java
+++ MVC_IndovinaN/src/controller/Controller.java
@@ -21,38 +21,49 @@ public class Controller implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+
 		int numTentativi = tentativi.getTentativi();
 		if (e.getActionCommand().equalsIgnoreCase("Invia")) {
 
-			String numeroTirato = grafica.getTxtNumero().getText();
-			int numTirato = Integer.parseInt(numeroTirato);
-			int flag = tentativi.checkNumber(numTirato);
-			
-			if (numTentativi >= 1) {
-				switch(flag) {
-				case 1:
-					tentativi.decrementaTentativi();
-					grafica.getLblTesto().setText("Prova con un numero minore !");
-					break;
-				case -1:
-					tentativi.decrementaTentativi();
-					grafica.getLblTesto().setText("Prova con un numero maggiore !");
-					break;
-				case 0:
-					grafica.getLblTesto().setText("Hai vinto :)");
-					break;
-				}
-				grafica.getLblScore().setText(""+numTentativi);
-			} 
-			else grafica.getLblTesto().setText("Hai perso :(");
+			int n = -99;
+			try {
+				
+				String numeroTirato = grafica.getTxtNumero().getText();
+				int numTirato = Integer.parseInt(numeroTirato);
+
+				int flag = tentativi.checkNumber(numTirato);
+
+				if (numTentativi >= 1) {
+					switch (flag) {
+					case 1:
+						tentativi.decrementaTentativi();
+						grafica.getLblTesto().setText("Prova con un numero minore !");
+						break;
+					case -1:
+						tentativi.decrementaTentativi();
+						grafica.getLblTesto().setText("Prova con un numero maggiore !");
+						break;
+					case 0:
+						grafica.getLblTesto().setText("Hai vinto :)");
+						break;
+					}
+					grafica.getLblScore().setText("" + numTentativi);
+				} else
+					grafica.getLblTesto().setText("Hai perso :(");
+
+			} catch (NumberFormatException ex) {
+				ex.printStackTrace();
+			}
+			if (n == -99) {
+				grafica.getLblTesto().setText("! Attenzione inserire valore numerico !");
+			}
 		}
-	
+
 		if (e.getActionCommand().equalsIgnoreCase("Reset")) {
-			
+
 			tentativi.generaNumero();
-			grafica.getLblScore().setText(""+numTentativi);
-			
+			grafica.getLblScore().setText("" + numTentativi);
+
 		}
 	}
 }
